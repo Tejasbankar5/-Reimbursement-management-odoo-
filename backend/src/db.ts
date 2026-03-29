96c8137344f2888ad278d144e3fef6911dce062f
Atharva Shinde <shindeatharva904@gmail.com>
2026-03-29 15:49:18 +0530

    update database config for Aiven cloud SSL

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,6 +5,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   dialect: 'mysql',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 export default sequelize;
